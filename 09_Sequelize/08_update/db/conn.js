9836c62c29d960c1684a02ca8aebd265b32f6b5f
JosephAlves0 <j.eduardo.alves@hotmail.com>
2023-06-14 18:27:50 -0300

    Modulo Sequelize

---
+++ 09_Sequelize/08_update/db/conn.js
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('nodesequelize2', 'root', '********', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+/*try {
+    sequelize.authenticate();
+    console.log('Conectamos com sucesso com o Sequeliez!');
+
+}catch(err) {
+    console.log('Não foi possível conectar: ', err)
+}*/
+
+export default sequelize;
